5c0b4a88a6ac060f0e67990b25778451f800a8e0
adantuon <adantuon@mail.umw.edu>
2025-06-07 13:17:58 -0400

    Added GameLogic class framework

--- CPSC440_Assignment3/GameLogic.h
+++ CPSC440_Assignment3/GameLogic.h
@@ -4,3 +4,15 @@
 #include <allegro5\allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+
+class game {
+	public:
+		game();
+		int getScore() { return score; }
+		void addScore() { score++; }
+		int getLives() { return lives; }
+		void removeLife() { lives--; }
+	private:
+		int score;
+		int lives;
+};
